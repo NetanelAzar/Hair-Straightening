a7489ddbf94f71b8d7bcb8408d4345778a93946c
AZAR <49813490+NetanelAzar@users.noreply.github.com>
2024-11-12 15:56:53 +0200

    modal

--- JS/Modal.js
+++ JS/Modal.js
@@ -1,6 +1,15 @@
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function () {
+    // יצירת המודל
+    var myModal = new bootstrap.Modal(document.getElementById('specialOfferModal'));
+
+    // המתן 10 שניות ואז פתח את המודל
     setTimeout(function() {
-        var myModal = new bootstrap.Modal(document.getElementById('specialOfferModal'));
         myModal.show();
-    }, 10000); // הצגת המודל אחרי 10 שניות
-};
+    }, 10000); // 10000 מילישניות = 10 שניות
+
+    // סגירת המודל בעזרת כפתור
+    var closeBtn = document.querySelector('.close-btn');
+    closeBtn.addEventListener('click', function() {
+        myModal.hide();
+    });
+});
